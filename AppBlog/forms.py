d526853e569dc3861f2a2e72ef6ea9a7f8d4b556
heber <hebermrosales@gmail.com>
2023-05-21 14:14:47 -0300

    creando fomrulario

--- AppBlog/forms.py
+++ AppBlog/forms.py
@@ -3,7 +3,7 @@ from django import forms
 class CrearArticulo(forms.Form):
    titulo = forms.CharField(max_length=64)
    subtitulo= forms.CharField(max_length=150)
-   cuerpo= forms.Textarea()
+   cuerpo= forms.CharField(widget=forms.Textarea())
    Autor = forms.CharField(max_length=64)
    fecha= forms.DateField(required=False)
    #imagen
