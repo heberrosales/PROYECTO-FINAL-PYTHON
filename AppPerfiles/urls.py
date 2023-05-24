c133d1ffaa9b042c2ce6f9602e99fb2da2914541
heber <hebermrosales@gmail.com>
2023-05-24 18:12:33 -0300

    agregado login,logout, registro

--- AppPerfiles/urls.py
+++ AppPerfiles/urls.py
@@ -3,4 +3,7 @@ from AppPerfiles import views
 
 urlpatterns = [
     path('registro/', views.registro, name="registro"),
+    path('login/', views.login_view, name="login"),
+    path('logout/', views.CustomLogoutView.as_view, name="logout"),
+
 ]
